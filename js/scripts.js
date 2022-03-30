366c2f7b5e13f6823b2d7c8b574d59ca85fae370
Julie H Shin <hyunhee26@gmail.com>
2022-03-30 01:35:47 -0500

    updatedddd

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,7 @@
 console.log("This is Julie. Nice to meet you!")
-var input = $("input");
+
 var containers = $(".container");
-function menuToggle() {
-  var x = document.getElementById ('myNavtoggle');
-  x.className += ' responsive'
-}
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
